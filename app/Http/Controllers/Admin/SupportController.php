387aa7cca990e2d9e05595f8db7c175cf866d342
nicolas <nicolasvettor@gmail.com>
2023-12-12 13:12:03 -0300

    chore: update and delete

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -23,6 +23,7 @@ class SupportController extends Controller
         }
         return view('admin/supports/show', compact ('support'));
     }
+
     
     public function create()
     {
@@ -39,4 +40,35 @@ class SupportController extends Controller
         return redirect()->route('supports.index');
     }
 
+    public function edit(Support $support, string|int $id)
+    {
+        if(!$support = $support->where('id', $id)->first()){
+            return back();
+        }
+
+        return view('admin/supports.edit', compact('support'));
+    }
+
+    public function update(Request $request, Support $support, string $id)
+    {
+        if(!$support = $support->find($id)){
+            return back();
+        }
+
+        $support->update($request->only([
+            'subject', 'body'
+        ]));
+
+        return redirect()->route('supports.index');
+    }
+
+    public function destroy(Support $support,string|int $id)
+    {
+        if(!$support = $support->find($id)){
+            return back();
+        }
+        $support->delete();
+
+        return redirect()->route('supports.index');
+    }
 }
