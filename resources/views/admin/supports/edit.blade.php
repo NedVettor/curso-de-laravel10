068cc85f5a16d6f5453fcd690c6daf45ab780b45
nicolas <nicolasvettor@gmail.com>
2024-01-05 10:39:05 -0300

    task:: create components to simplify the views

--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,16 +1,10 @@
 <h1>Dúvida {{$support->id}}</h1>
 
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        {{ $error }}
-    @endforeach
-@endif
+<x-alert></x-alert>
 
 <form action="{{ route('supports.update', $support->id)}}" method="POST">
-    @csrf()
-    <!-- <input type="text" value="PUT" name="_method"> -->
     @method('PUT')
-    <input type="text" placeholder="Assunto" name="subject" value="{{$support->subject}}">
-    <textarea name="body" id="" cols="30" rows="5" placeholder="Descrição">{{$support->body}}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.supports.partials.form', [
+        'support' => $support
+    ])
 </form>
