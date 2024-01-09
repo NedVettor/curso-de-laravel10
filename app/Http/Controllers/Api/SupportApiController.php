d144ff2d6bf0bf5742eabbdcdd2234dbbcc83849
nicolas <nicolasvettor@gmail.com>
2024-01-09 13:48:59 -0300

    add:: apidapter

--- app/Http/Controllers/Api/SupportApiController.php
+++ app/Http/Controllers/Api/SupportApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Adapters\ApiAdapter;
 use App\DTO\Supports\CreateSupportDTO;
 use App\DTO\Supports\UpdateSupportDTO;
 use App\Http\Controllers\Controller;
@@ -33,20 +34,7 @@ class SupportApiController extends Controller
             totalPerPage: $request->get('per_page', 3), 
             filter: $request->filter
         );
-
-
-        return SupportResource::collection($supports->items())
-                                ->additional([
-                                    'meta' => [
-                                        'total' => $supports->total(),
-                                        'is_first_page' => $supports->isFirstPage(),
-                                        'is_last_page' => $supports->isLastPage(),
-                                        'current_page' => $supports->currentPage(),
-                                        'next_page' => $supports->getNumberNextPage(),
-                                        'previus_page' => $supports->getNumberPreviusPage(),
-
-                                    ]
-                                ]);
+    return ApiAdapter::toJson($supports);
     }
 
     /**
